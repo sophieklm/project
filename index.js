7c0b85b1f253f121da559407bf60ee384defbb8e
Sophie Mann <sklm@live.co.uk>
2020-05-17 14:02:19 +0100

    Add socket server

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`App listening on port ${port}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("connected");
+  socket.emit("news", { hello: "world" });
+  socket.on("my other event", (data) => {
+    console.log(data);
+  });
 });
